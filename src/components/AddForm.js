f31113841d60b67c4cb982f0281e9b607f64e88a
Juan Diego Guerra <jdxelric@gmail.com>
2023-08-24 03:32:57 -0500

    Update AddForm.js

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,29 +1,49 @@
-import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { addBook } from '../redux/books/booksSlice';
 
-const AddForm = ({
-  handleAdd, handleTitle, handleAuthor, title, author,
-}) => (
-  <form onSubmit={handleAdd}>
-    <label htmlFor="bookTitle">
-      Title
-      <input type="text" id="bookTitle" placeholder="Title" value={title} onChange={handleTitle} />
-    </label>
-    <br />
-    <label htmlFor="bookAuthor">
-      Author
-      <input type="text" id="bookAuthor" placeholder="Author" value={author} onChange={handleAuthor} />
-    </label>
-    <br />
-    <button type="submit">Add Book</button>
-  </form>
-);
+const AddForm = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const dispatch = useDispatch();
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleAuthorChange = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    const newBook = {
+      itemId: uuidv4(),
+      title,
+      author,
+    };
+    dispatch(addBook(newBook));
+    setTitle('');
+    setAuthor('');
+  };
+
+  return (
+    <form onSubmit={handleAdd}>
+      <label htmlFor="bookTitle">
+        Title
+        <input type="text" id="bookTitle" placeholder="Title" value={title} onChange={handleTitleChange} />
+      </label>
+      <br />
+      <label htmlFor="bookAuthor">
+        Author
+        <input type="text" id="bookAuthor" placeholder="Author" value={author} onChange={handleAuthorChange} />
+      </label>
+      <br />
+      <button type="submit">Add Book</button>
+    </form>
+  );
+};
 
 export default AddForm;
-
-AddForm.propTypes = {
-  handleAdd: PropTypes.func.isRequired,
-  handleTitle: PropTypes.func.isRequired,
-  handleAuthor: PropTypes.func.isRequired,
-  title: PropTypes.string.isRequired,
-  author: PropTypes.string.isRequired,
-};
