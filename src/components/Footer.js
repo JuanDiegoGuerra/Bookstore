72cffdd418a5f1430d78fc0aeed35e911fe68359
Juan Diego Guerra <jdxelric@gmail.com>
2023-08-25 09:36:54 -0500

    Fixing Linters

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 const Footer = () => (
-<p className="footer-container">Developed by JD Guerra. © 2023.</p>
+  <p className="footer-container">Developed by JD Guerra. © 2023.</p>
 );
 
 export default Footer;
